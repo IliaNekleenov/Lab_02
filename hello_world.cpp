db6994b67c788b17981e8e47a5f5fa02b7293733
IliaNekleenov <47657009+IliaNekleenov@users.noreply.github.com>
2019-05-24 19:39:19 +0300

    change comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
-//Comment1
+//Comment1, changed
 #include <iostream>
 #include <string>
 int main(){
 std::string name;
-std::cin>>name;//Comment2
+std::cin>>name;//Comment2, changed
 std::cout<<"Hello World from "<<name;
 }
